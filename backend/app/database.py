ec45f5b8cbe28e576662145de551ad8974701d55
Karthik Mukesh <karthikmukesh19@gmail.com>
2026-07-29 15:55:13 +0530

    Configure backend for Vercel

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-client = MongoClient("mongodb://localhost:27017")
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
 
 db = client["shadowwatch"]
 
